e5927165d59265806adee3e55de0536ae4c71a65
wits <wits9747@gmail.com>
2024-05-21 16:14:28 +0900

    admin 추가

---
+++ starmakers/src/main/java/com/aloha/starmakers/Controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+    
+    @GetMapping("")
+    public String getMethodName() {
+        return "admin/index";
+    }
+
+    @GetMapping("/pages/{path}")
+    public String getMethodName2(@PathVariable("path") String path) {        
+        return "admin/pages/"+path;
+    } 
+}
