db6a794b3f2c3542cad0499702bd23c9dc197928
Sin Jun Su <tlswnstn0191@naver.com>
2024-05-21 17:09:25 +0900

    다시

--- starmakers/src/main/java/com/aloha/starmakers/Controller/AdminController.java
+++ starmakers/src/main/java/com/aloha/starmakers/Controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controller;
+package com.aloha.starmakers.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
