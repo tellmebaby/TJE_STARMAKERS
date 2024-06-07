e0d2d4ac68373986378c7cf2e21558b82c92120e
Joyeeeeeeeeeee <160221951+Joyeeeeeeeeeee@users.noreply.github.com>
2024-06-07 19:40:38 +0900

    admin 회원관리 수정

--- starmakers/SQL/query.sql
+++ starmakers/SQL/query.sql
@@ -12,7 +12,7 @@ select * from `user`;
 select * from `user_auth`;
 
 INSERT INTO user_auth( user_id, auth )
-        VALUES ( 'ab@afdsf.com' , 'ADMIN' );
+        VALUES ( 'ab@afdsf.com' , 'ROLE_USER' );
 
 delete from user_auth where auth_no = 2;
 
